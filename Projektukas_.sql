47cecfc2d065d83b4e29ed4eb07211b60af2fc15
GediminasNo <106587594+GediminasNo@users.noreply.github.com>
2023-09-22 14:34:43 +0300

    Update Projektukas_.sql

--- Projektukas_.sql
+++ Projektukas_.sql
@@ -49,9 +49,10 @@ alter table order_items
 add foreign key (promotion_id)  references promotions (promotion_id);
 
 -- 
--- 3 Užduotis
+-- 3 Užduotis   -- 3 Tasks
 
--- 3.1. Kiekvieną savaitę gautų užsakymų skaičių.
+-- 3.1. Kiekvieną savaitę gautų užsakymų skaičių.   -- 3.1. Number of orders received each week.
+
 
 select c.calendar_week as savaites,  count(*),c2.first_name ,  c2.last_name 
 from calendar c
@@ -62,7 +63,7 @@ group by c.calendar_id,c2.first_name ,  c2.last_name
 order by c.calendar_week 
 
 
--- 3.2. Populiariausią, kiekvienos savaitės prekę.
+-- 3.2. Populiariausią, kiekvienos savaitės prekę.   -- 3.2. The most popular product of each week.
 
 
 select * from products p 
@@ -74,7 +75,7 @@ left join calendar c on oi.calendar_id = c.calendar_id
 group by p.name,c.calendar_week 
 
 
--- 3.3Kiekvieno mėnesio, kiekvienos Category_level1_name kategorijos pajamas
+-- 3.3Kiekvieno mėnesio, kiekvienos Category_level1_name kategorijos pajamas   -- 3.3Revenues for each month, for each Category_level1_name category
 
 select c.calendar_month,sum(oi.amount_without_vat) as pajamos, c2.cat_level_1_name
 from calendar c 
@@ -85,7 +86,7 @@ where oi.amount_without_vat is not null and c2.cat_level_1_name is not null
 group by c.calendar_month ,c2.cat_level_1_name
 
 
--- 3.4Miestų pavadinimus ir ten gyvenančių klientų skaičių.
+-- 3.4Miestų pavadinimus ir ten gyvenančių klientų skaičių.   -- 3.4 Names of cities and the number of customers living there.
 
 select * from customers c 
 
@@ -94,8 +95,8 @@ from customers c
 group by c.city , c.first_name,c.last_name 
 
 
--- 3.5.Miestų pavadinimus ir į tuos miestus siunčiamų užsakymų skaičių.
--- a. Išskaidykite užsakymų skaičių pagal delivery status lauką.
+-- 3.5.Miestų pavadinimus ir į tuos miestus siunčiamų užsakymų skaičių.       -- 3.5. Names of cities and the number of orders sent to those cities.
+-- a. Išskaidykite užsakymų skaičių pagal delivery status lauką.              -- a. Break down the number of orders by the delivery status field.
 
 
 select d.city, count(d.delivery_id) as uzsakymu_skaicius, d.delivery_status 
@@ -105,7 +106,7 @@ group by d.city ,d.delivery_status
 order by d.delivery_status  asc
 
 
--- 3.6.Raskite, kuri akcija sugeneravo daugiausiai pajamu.
+-- 3.6.Raskite, kuri akcija sugeneravo daugiausiai pajamu.             -- 3.6. Find which share generated the most income.
 
 
 select  p.name , sum(p.cost) as maksimali_suma , count(*) as uzsakymai
@@ -116,8 +117,8 @@ order by maksimali_suma desc
 limit 1 ;
 
 
--- 3.7. Raskite, kuri akcija sugeneravo daugiausiai nuolaidos.
-
+-- 3.7. Raskite, kuri akcija sugeneravo daugiausiai nuolaidos.         -- 3.7. Find which promotion generated the most discount.
+ 
 select p.name ,sum(p2.discount) as nuolaida_masimali
 from products p 
 left join order_items oi on p.product_id = oi.product_id 
@@ -127,14 +128,15 @@ order by nuolaida_masimali desc
 limit 1 ;
 
 
--- 4 Užduotis
+-- 4 Užduotis         -- 4 Task
 
--- Sukurkite laikinas lenteles.
+-- Sukurkite laikinas lenteles.         -- Create temporary tables.
 
 
--- 1. Lentelė, kuri palygina kaip keitėsi praėjusios savaitės pajamos 
--- lyginant su prieš ją ėjusia
--- savaite. Pajamos išskaidytos pagal pirmo lygio kategorijas.
+-- 1. Lentelė, kuri palygina kaip keitėsi praėjusios savaitės pajamos                            -- 1. A table that compares how the income of the previous week changed
+-- lyginant su prieš ją ėjusia                                                                   -- compared to the one before it
+-- savaite. Pajamos išskaidytos pagal pirmo lygio kategorijas.                                   -- a week. Income is broken down by first-level categories.
+
 
 
 select c2.cat_level_1_name, c.calendar_week, sum(oi.amount_without_vat) as pajamos
@@ -165,9 +167,10 @@ group by c.calendar_week ,c2.cat_level_1_name;
 
 
 -- 
--- 4.2Lentelė, kuri palygina kaip keitėsi praėjusio mėnesio
--- pajamos lyginant su prieš jį ėjusiu
--- mėnesiu. Pajamos išskaidytos pagal pristatymo miestus.
+-- 4.2Lentelė, kuri palygina kaip keitėsi praėjusio mėnesio              -- 4.2 Table that compares how it changed last month
+-- pajamos lyginant su prieš jį ėjusiu                                   -- income compared to the previous one
+-- mėnesiu. Pajamos išskaidytos pagal pristatymo miestus.                -- a month. Revenue is broken down by delivery cities.
+
 
 select * from calendar c ;
 
@@ -189,9 +192,11 @@ order by city ;
 
 
 
--- 4.3. Lentelė, kuri lygina, kiek praėjusį mėnesį atsirado naujų klientų
---  lyginant su prieš jį ėjusiu
--- mėnesiu.
+-- 4.3. Lentelė, kuri lygina, kiek praėjusį mėnesį atsirado naujų klientų                -- 4.3. A table that compares how many new customers were added last month
+--  lyginant su prieš jį ėjusiu                                                          -- compared to his predecessor
+-- mėnesiu.                                                                              -- a month.
+
+
 select * from calendar c ;
 
 select c.calendar_month , count(c2.customer_id) as klientu_pokytis
@@ -211,13 +216,14 @@ order by klientu_pokytis asc
 
 
 
--- 5 Užduotis
+-- 5 Užduotis                                                         -- 5 Tasks
+  
+-- Sukurkite procedūras.                                              -- Create procedures.
 
--- Sukurkite procedūras.
+-- 5.1.Procedūrą, kuriai padavus datą, jinai grąžintų tos datos       -- 5.1. The procedure, for which, after submitting a date, it would return that date
 
--- 5.1.Procedūrą, kuriai padavus datą, jinai grąžintų tos datos
-
--- a.Visų užsakymų skaičių.
+-- a.Visų užsakymų skaičių.                                           -- a. The number of all orders.
+  
 use naujas_projektas_1_1;
 
 
@@ -236,7 +242,7 @@ select * from calendar c ;
 call klientu_uzsakymas_data('2021-01-24'); 
 
 
--- b. Visų užsakymų pajamas.
+-- b. Visų užsakymų pajamas.                            -- b. Revenue of all orders.
 
 
 select c.calendar_date , c1.customer_id , 
@@ -254,7 +260,7 @@ order by c.calendar_date asc;
 call klientu_uzsakymas_pajamos_1('2021-03-13');
 
 
--- c. Visų, tos dienos klientų skaičių.
+-- c. Visų, tos dienos klientų skaičių.                  -- c. Total number of customers for that day.
 
 
 select c.calendar_date , count(c1.customer_id) as klientu_skaicius
@@ -272,9 +278,9 @@ call klientu_skaicius('2021-03-07');
 
 
 
--- 5.2. Procedūrą, kuriai padavus kliento id, jinai grąžintų
+-- 5.2. Procedūrą, kuriai padavus kliento id, jinai grąžintų                -- 5.2. A procedure that, given the client id, would return it
 
--- a. Visų kliento užsakymų skaičių.
+-- a. Visų kliento užsakymų skaičių.                                        -- a. Number of all customer orders.
 
 select c.customer_id , count(*) kliento_uzsakymai
 from customers c 
@@ -285,7 +291,7 @@ order by kliento_uzsakymai asc;
 call klientu_uzsakymai_su_id(55);
 
 
--- b. Visų užsakymų pajamas
+-- b. Visų užsakymų pajamas                                                -- b. Revenue of all orders
 
 select  count(*), sum(oi.amount_without_vat) as pajamos
 from customers c 
@@ -296,7 +302,7 @@ order by pajamos desc;
 call uzsakymu_pajamos(10);
 
 
--- c. Vidutinę užsakymo vertę.
+-- c. Vidutinę užsakymo vertę.                                              -- c. Average order value.
 
 
 select  count(*) as uzsakymai, avg(oi.amount_without_vat) as vidutine_verte_uzsakymo
@@ -310,12 +316,12 @@ call vidutine_verte_uzsakymo(10) ;
 
 
 
--- d. Vidutinį užsakytų prekių skaičių.
+-- d. Vidutinį užsakytų prekių skaičių.                                      -- d. Average number of items ordered.
 
 
 
 
--- e. Paskutinio užsakymo datą.
+-- e. Paskutinio užsakymo datą.                                              -- e. The date of the last order.
 
 select c. created_date
 from customers c 
@@ -324,7 +330,7 @@ order by c.created_date desc limit 1;
 call paskutine_data(4) 
 
 
--- f. Kliento paskyros sukūrimo amžių dienomis.
+-- f. Kliento paskyros sukūrimo amžių dienomis.                               -- f. In the age days of the creation of the customer account.
 
 
 select created_date,
